690982dcbf7364395e337efa3210c3f5351a0d20
jonathan <jonathancevv@gmail.com>
2022-05-06 03:32:55 -0500

    se creó la clase usuario

---
+++ src/main/java/Model/login/UsuarioRol.java
@@ -0,0 +1,16 @@
+package Model.login;
+
+import Model.administracion.Usuario;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class UsuarioRol {
+    private int idUsuarioRol;
+    private Rol rol;
+    private Usuario usuario;
+    private Date fechaRegistro;
+}
